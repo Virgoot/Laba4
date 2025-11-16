1c60af58c9c100aa6d2c5057670748c0f7a3d5e5
Virgoot <gsecond77@gmail.com>
2025-11-16 08:16:18 +0400

    Upgraded Rook class for better readability

--- Rook.cs
+++ Rook.cs
@@ -19,56 +19,41 @@ class Rook : Piece
         int endRow = 8 - int.Parse(end[1].ToString());
         int endColumn = end[0] - 'A';
 
+        int rowDiff = Math.Abs(endRow - startRow);
+        int colDiff = Math.Abs(endColumn - startColumn);
 
 
         if (startRow != endRow && startColumn != endColumn)
-        {
             return false;
-        }
-
-
-        /*
-            start row | *          ~ | end row
-        */
-
-        if (startRow == endRow)
-        {
-            int step = (endColumn > startColumn) ? 1 : -1;
-
-            for (int n = startColumn + step; n != endColumn; n += step)
-            {
-                if (board[startRow, n] != null)
-                {
-                    Console.WriteLine("Путь заблокирован");
-                    return false;
-                }
-            }
-        }
-
-        if (startColumn == endColumn)
-        {
-            int step = (endRow > startRow) ? 1 : -1;
-
-            for (int n = startRow + step; n != endRow; n += step)
-            {
-                if (board[n, startColumn] != null)
-                {
-
-                    Console.WriteLine("Путь заблокирован");
-                    return false;
-                }
-            }
-        }
-
-        Piece target = board[endRow, endColumn];
-
-        if (target != null && target.Color == Color)
-        {
-            Console.WriteLine("Вы не можете съесть свою фигуру");
-            return false;
-        }
-
         
-        return true;
+        int rowStep = (endRow == startRow) ? 0 : (endRow > startRow ? 1 : -1);
+        int colStep = (endColumn == startColumn) ? 0 : (endColumn > startColumn ? 1 : -1);
+
+
+        int currentRow = startRow + rowStep;
+        int currentCol = startColumn + colStep;
+
+        while(currentRow != endRow || currentCol != endColumn)
+        {
+            
+            if (board[currentRow, currentCol] != null) {
+                return false;
+            }
+            
+            currentRow += rowStep;
+            currentCol += colStep;   
+        }
+
+
+        if ((rowDiff > 0 && colDiff == 0) || (colDiff > 0 && rowDiff == 0))
+        {
+            Piece target = board[endRow, endColumn];
+            if (target == null || target.Color != this.Color)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
